121015e7c954ef2a3a78d83d8cd0ae90744c98de
Capuno <c@capuno.cat>
2018-01-17 14:17:33 +0100

    Temporal colors

--- colors.hpp
+++ colors.hpp
@@ -2,9 +2,9 @@
 #define __FF_COLORS__
 
 #define TEXT_TITLE "\033[1m"
-#define TEXT_NORMAL "\033[m"
-#define BAR_USED "\033[100m"
-#define BAR_FREE "\033[47m"
+#define TEXT_NORMAL "\033[0m"
+#define BAR_USED "\033[47m"
+#define BAR_FREE "\033[100m"
 
 #define RESET "\033[0m"
 #define BOLD "\033[1m"
@@ -16,7 +16,6 @@
 #define BLUE "\033[34m"
 #define MAGENTA "\033[35m"
 #define CYAN "\033[36m"
-
 #define WHITE "\033[37m"
 
 #endif
